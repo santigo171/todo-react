cb9bdd38172f18ad887bee73c5388ca9ad01e29c
David Hurtado <santibetancourt17@gmail.com>
2022-03-31 21:25:16 -0500

    Updates

---
+++ precache-manifest.c76da04c9e98bbddda302e04839a6056.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c872a78a17bab038c7ff4856c4baaad",
+    "url": "/todo-react/index.html"
+  },
+  {
+    "revision": "f1bea19b8e3387f3b14d",
+    "url": "/todo-react/static/css/main.f2ee5f48.chunk.css"
+  },
+  {
+    "revision": "3c305bf0b1d37dd1ae0b",
+    "url": "/todo-react/static/js/2.931ed4fd.chunk.js"
+  },
+  {
+    "revision": "85ceabc36d7383162763218190a09a59",
+    "url": "/todo-react/static/js/2.931ed4fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1bea19b8e3387f3b14d",
+    "url": "/todo-react/static/js/main.a9429530.chunk.js"
+  },
+  {
+    "revision": "1614377f578425b5d648",
+    "url": "/todo-react/static/js/runtime-main.92cd31f8.js"
+  },
+  {
+    "revision": "c3cec71b9ad912b1e3afb0ebb04a2c10",
+    "url": "/todo-react/static/media/charles_darwin.c3cec71b.png"
+  },
+  {
+    "revision": "8fa572a3f1bbfd337696098aecef192a",
+    "url": "/todo-react/static/media/coco_chanel.8fa572a3.png"
+  },
+  {
+    "revision": "914624e7c1c2c741129f8cf8c2835a40",
+    "url": "/todo-react/static/media/coffee.914624e7.png"
+  },
+  {
+    "revision": "2b8e544ab6a1cbff563d074673d98062",
+    "url": "/todo-react/static/media/gretchen_rubin.2b8e544a.png"
+  },
+  {
+    "revision": "3fbaf6875f029f57b14bfd901944683d",
+    "url": "/todo-react/static/media/ladylike-bb.regular.3fbaf687.ttf"
+  },
+  {
+    "revision": "35e806eeefa3cc4dabc5bf1261eb3c80",
+    "url": "/todo-react/static/media/lens.35e806ee.svg"
+  },
+  {
+    "revision": "d9b93ec645ae69cba6f406db4f937946",
+    "url": "/todo-react/static/media/martin_luther_king.d9b93ec6.png"
+  },
+  {
+    "revision": "3f0ffc5b2622a3b6869cce2b02b9b8d4",
+    "url": "/todo-react/static/media/paper_texture_2.3f0ffc5b.jpg"
+  },
+  {
+    "revision": "cb8247f76958ab734e6ec63efc6fbb63",
+    "url": "/todo-react/static/media/pumpkin-cheesecake.cb8247f7.otf"
+  },
+  {
+    "revision": "37650ce3946b854c663b57ed510374c2",
+    "url": "/todo-react/static/media/thinking.37650ce3.gif"
+  },
+  {
+    "revision": "2db3f1ba4e2835d08ce4c90fadefa665",
+    "url": "/todo-react/static/media/tim_ferriss.2db3f1ba.png"
+  },
+  {
+    "revision": "4f6bd90405898d164bcaf28ceb986312",
+    "url": "/todo-react/static/media/tony_robbins.4f6bd904.png"
+  },
+  {
+    "revision": "d3a4976d3bb77feacceda6b8063a33b2",
+    "url": "/todo-react/static/media/walking.d3a4976d.gif"
+  },
+  {
+    "revision": "adb0cd98dd16e35dffaf5889199d6a76",
+    "url": "/todo-react/static/media/walt_disney.adb0cd98.png"
+  },
+  {
+    "revision": "88508db8c1d05f3522a06495f9b9c454",
+    "url": "/todo-react/static/media/wooden_background.88508db8.jpg"
+  }
+]);
